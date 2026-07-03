03f6450595c14f1a40918afb0a5137391ccbda5e
soumyasamal23 <38082044+soumyasamal23@users.noreply.github.com>
2026-07-03 21:09:22 +0530

    Complete Multi AI Agent code

---
+++ main.py
@@ -0,0 +1,8 @@
+from src.pipelines.pipeline import run_research_pipeline
+
+topic = "The impact of AI on the job market in 2026"
+
+# run_research_pipeline(topic)
+
+if __name__ == "__main__":
+    run_research_pipeline(topic)
